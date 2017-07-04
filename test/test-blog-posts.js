d5e7c63d05a7c05d23b48611a6bad9e7a79cb577
stevengalvis <steven.galvis@gmail.com>
2017-07-04 18:38:26 -0400

    added DELETE endpoint

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -156,4 +156,28 @@ describe('BlogPosts API resource', function() {
 
   });
 
-})
+describe('DELETE endpoint', function() {
+
+  it('should delete a post by id', function() {
+    let post;
+
+    return BlogPost
+      .findOne()
+      .exec()
+      .then(function(_post) {
+        post = _post;
+        return chai.request(app).delete(`/posts/${post.id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+        return BlogPost.findById(post.id).exec();
+      })
+      .then(function(_post) {
+        should.not.exist(_post);
+      });
+
+  })
+
+});
+
+});
