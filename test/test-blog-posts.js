ff9c40906f4b4ec230413dc4dbc27a5d7b22c44c
stevengalvis <steven.galvis@gmail.com>
2017-07-04 17:14:05 -0400

    imported interfaces

---
+++ test/test-blog-posts.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const should = chai.should();
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../sever');
+const {TEST_DATABASE_URL} = require('../config');
