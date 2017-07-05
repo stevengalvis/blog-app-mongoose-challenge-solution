ced624e6697337eec4499ec7550eae75e3ca03d4
stevengalvis <steven.galvis@gmail.com>
2017-07-05 19:28:50 -0400

    minor fixes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blogs-app';
+                      'mongodb://localhost/blog-app';
 
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
                               'mongodb://localhost/test-blogs-app');
